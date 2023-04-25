dec45e6be243098ca2adba3acfe3bee80788b758
MartinDePascali <martin.depascali@davinci.edu.ar>
2023-04-25 03:16:39 -0300

    fetch: sw installa check

--- sw.js
+++ sw.js
@@ -1,6 +1,40 @@
 console.log("SW: I'm a Service Worker!");
 
+//Ciclos service workers
+
+//Chequear instalación
+self.addEventListener('install', event => {
+    console.log("SW: ", event);
+
+    const install = new Promise((resolve, reject) => {
+        //La instalación involucra muchas cosas, por lo que se simula la espera de recursos con el timeout
+        setTimeout(() => {
+            console.log("SW: Install complete!");
+            self.skipWaiting();
+            resolve();
+        }, 500);
+    });
+    //Esperar a que se complete la instalación
+    event.waitUntil(install);
+});
+
 self.addEventListener('fetch', event => {
     console.log("SW: ", event.request.url);
     console.log(event);
+
+    //Chequear si la url que consulto es la de la API
+    if(event.request.url.includes('https://reqres.in')){
+        //Si la url incluye el dominio de la API
+        const response = new Response(`{ 
+            "ok": false, 
+            "data": [],
+            "message": "No se puede acceder a la API" 
+        }`, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        event.respondWith(response);
+    }
 });
